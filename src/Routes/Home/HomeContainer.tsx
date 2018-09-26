2769d224c509fc238a5cf8c9c5c81093ed20ee8c
seunghoon <santony.choi@gmail.com>
2018-09-27 00:05:25 +0900

    Basic tree

---
+++ src/Routes/Home/HomeContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "../../typed_components";
+
+class HomeContainer extends React.Component {
+    public render() {
+        styled.div`
+
+        `
+    }
+    }
+}
+
+export default HomeContainer;
